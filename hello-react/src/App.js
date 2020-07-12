8a011167ba95cb3c5aaaf946ebab6949d937bd74
developer-do <dev-do@naver.com>
2020-07-13 00:40:07 +0900

    컴포넌트에 ref 달기 부모에서 자식으로 스크롤 이벤트 전달

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 
 const onClick = () => {
@@ -12,10 +13,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <MyComponent name="React" age={29} onClick={onClick} />
+        <MyComponent name="React" age={29} onClick={onClick} ref={(ref) => {this.MyComponent = ref}}/>
         <EventPractice />
         <hr />
         <ValidationSample />
+        <hr />
+        <ScrollBox ref={(ref) => {
+          this.scrollBox = ref;
+          console.log(ref);
+        }}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
       </div>
     );
   }
