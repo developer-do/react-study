41435768d5afdb0baa8fed05ccaa3b9295666901
developer-do <dev-do@naver.com>
2020-07-12 13:02:31 +0900

    event handler 공부 / input 여러개 핸들링 하기

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react'
 
 class EventPractice extends Component {
     state = {
-        message: ''
-    } 
+        message: '',
+        username: '',
+        age: ''
+    }
+
+    handleChange = (e) => {
+        console.log([e.target.name]);
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + '은 ' + this.state.age + ' : ' +this.state.message);
+        this.setState({
+            username: '',
+            message: '',
+            age: ''
+        });
+    }
 
     render() {
         return (
@@ -11,26 +29,26 @@ class EventPractice extends Component {
                 <h1>이벤트 연습</h1>
                 <input
                     type="text"
+                    name="username"
+                    placeholder="유저명"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="age"
+                    placeholder="나이를 입력하세요."
+                    value={this.state.age}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
                     name="message"
                     placeholder="아무거나 입력해 보세요."
                     value={this.state.message}
-                    onChange={
-                        (e) => {
-                            console.log(e.target.value);
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    onChange={this.handleChange}
                 />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>확인</button>
+                <button onClick={this.handleClick}>확인</button>
                 <p>{this.state.message}</p>
             </div>
         )
